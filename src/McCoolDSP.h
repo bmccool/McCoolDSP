8c75b041130f34837cc7bdae7ccb8d1f9c68a37e
Brendon McCool <brendonmccool@gmail.com>
2022-08-17 22:39:13 -0400

    First working HPS

--- src/McCoolDSP.h
+++ src/McCoolDSP.h
@@ -1,8 +1,10 @@
 #pragma once
 
 #include <boost/circular_buffer.hpp>
+#include "LomontFFT.h"
 
 #define PI 3.141592653589793238462643383279502884197169399375105820974944
+#define WHITE_NOISE_THRESH 0.2
 
 #ifdef WIN32
     #define McCoolDSP_EXPORT __declspec(dllexport)
@@ -15,14 +17,28 @@ McCoolDSP_EXPORT void McCoolDSP();
 
 class HPS_Frequency_Detector {
     private:
+        LomontFFT LFFT;
 
     public:
-        unsigned int window_size, num_products;
+        unsigned unsigned int window_size, num_products;
         // Create a circular buffer with a capacity for 3 integers.
-        boost::circular_buffer<int> samples;
-        HPS_Frequency_Detector(unsigned int window_size, unsigned int num_products): window_size(window_size), num_products(num_products), samples(boost::circular_buffer<int>(window_size)){}
+        //boost::circular_buffer<double> samples;
+        std::vector<double> samples;
+
+        
+        HPS_Frequency_Detector(unsigned int window_size, unsigned int num_products): window_size(window_size), num_products(num_products){ LFFT.Initialize(window_size); samples.reserve(window_size * num_products / 2); }
         void add_sample(int sample){
             samples.push_back(sample);
+        }
+
+        void FFT(){
+            LFFT.A = 1;
+            LFFT.B = 1;
+            LFFT.RealFFT(samples, true);
+        }
+
+        void MAG(){
+            LFFT.mag(samples);
         }
 
         void hanning(){
@@ -39,6 +55,10 @@ class HPS_Frequency_Detector {
                 window[i] = 0.5 - 0.5*(cos(2*PI*i / (length - 1)));
                 samples[i] = samples[i] * (0.5 - 0.5*(cos(2*PI*i / (length - 1))));
             }
+        }
+
+        void normalize(){
+
         }
 
         float detect(){
@@ -62,6 +82,7 @@ class HPS_Frequency_Detector {
             9. Find the Max index, the index with the highest value in the result
             10. Find the Max Frequency, as determined by max_index * (SAMPLE_FREQ / WINDOW_SIZE) / NUM_HPS
             */
+           std::vector<double> OCTAVE_BANDS = {50, 100, 200, 400, 800, 1600, 3200, 6400, 12800, 25600};
 
             // Step 0 - Precheck
             if (samples.size() < window_size){
@@ -75,30 +96,79 @@ class HPS_Frequency_Detector {
             hanning();
 
             // Step 3 - Apply DFT
-            // TODO
+            FFT();
+            // Step 3.5 Combine real/complex parts into one magnitude, reduce size to 1/2
+            MAG();
 
             // Step 4 - Suppres mains hum
-            // TODO
+            double DELTA_FREQ = 3300 / window_size; // frequency step width of the interpolated DFT
+            for(int i = 0; i < (int)(62 / DELTA_FREQ); i++){  // 62 is mains hum
+                samples[i] = 0;
+            }
 
             // Step 5 - Remove noise from each frequency band
-            // TODO
+            // TODO This doesn't EXACTLY match the python version...
+            for(int j = 0; j < OCTAVE_BANDS.size() - 1; j++){
+                auto ind_start = (int)(OCTAVE_BANDS[j] / DELTA_FREQ);
+                auto ind_end = (int)(OCTAVE_BANDS[j+1] / DELTA_FREQ);
+                if(ind_end > samples.size()){ ind_end = samples.size(); }
+                double norm = 0;
+                for(int i = ind_start; i < ind_end; i++){
+                    norm += (pow(abs(samples[i]), 2));
+                }
+                norm = pow(norm, (1./2));
+                auto average = pow(norm, 2);
+                average /= (ind_end-ind_start);
+                average = pow(average, 0.5);
+                for(int i = ind_start; i < ind_end; i++){
+                    if(samples[i] < (WHITE_NOISE_THRESH * average)){ samples[i] = 0; }
+                }
+            }
 
             // Step 6 - Interpolate Smoothly
-            // TODO
+            int size = window_size/2;
+            samples.resize((size * 3) - 2); // TODO magic number HPS products, 2 is because we take off 3 - 1 at the end
+            for(int i = size-1; i > 0; i--){
+                samples[(i*3) - 0] = samples[i];
+                samples[(i*3) - 1] = samples[i] - ((samples[i] - samples[i-1]) / 3);
+                samples[(i*3) - 2] = samples[i] - ((samples[i] - samples[i-1])  * 2 / 3);
+            }
 
             // Step 7 - Normalize
-            // TODO
+            double norm = 0;
+            for(int i = 0; i < samples.size(); i++){
+                norm += (pow(abs(samples[i]), 2));
+            }
+            norm = pow(norm, (1./2));
+            for(int i = 0; i < samples.size(); i++){
+                samples[i] = samples[i] / norm;
+            }
 
             // Step 8 - Multiply the harmonic spectrums
-            // TODO
+            for(int i = 0; i < samples.size(); i++){
+                //for(int j = 0; j < num_products){
+                if(i < (samples.size() / num_products)){
+                    samples[i] = samples[i] * samples[i*2] * samples[i*3];
+                }
+                else { samples[i] = 0; }
+                //}
+
+            }
 
             // Step 9 - Get Max Index
-            // TODO
+            double max_value = 0;
+            auto max_index = -1;
+            for(int i = 0; i < samples.size(); i++){
+                if(samples[i] > max_value){
+                    max_value = samples[i];
+                    max_index = i;
+                }
+            }
 
             // Step 10 - Translate Index to Frequency
-            // TODO
+            double max_frequency = max_index * (3300.0/window_size) / 3.0;
 
-            return 0;
+            return max_frequency;
         }
 
 
--- test_package/test.cpp
+++ test_package/test.cpp
@@ -19,30 +19,31 @@ TEST_CASE("First Test", "[test]"){
 TEST_CASE("HPS_Frequency_Detector", "[test]"){
     HPS_Frequency_Detector detector(256, 3);
     REQUIRE(detector.window_size == 256);
+    REQUIRE(detector.samples.empty() == true);
     REQUIRE(detector.samples.size() == 0);
-    REQUIRE(detector.samples.reserve() == 256);
+    //REQUIRE(detector.samples.reserve() == 256);
     REQUIRE(detector.num_products == 3);
 }
 
 TEST_CASE("HPS_Frequency_Detector::samples", "[test]"){
     HPS_Frequency_Detector detector(10, 3);
     REQUIRE(detector.samples.size() == 0);
-    REQUIRE(detector.samples.reserve() == 10);
-    for(int i = 0; i < 15; i++){
-        detector.add_sample(i);
-    }
-    REQUIRE(detector.samples.size() == 10);
-    REQUIRE(detector.samples.reserve() == 0);
-    REQUIRE(detector.samples[0] == 5);
-    REQUIRE(detector.samples[1] == 6);
-    REQUIRE(detector.samples[2] == 7);
-    REQUIRE(detector.samples[3] == 8);
-    REQUIRE(detector.samples[4] == 9);
-    REQUIRE(detector.samples[5] == 10);
-    REQUIRE(detector.samples[6] == 11);
-    REQUIRE(detector.samples[7] == 12);
-    REQUIRE(detector.samples[8] == 13);
-    REQUIRE(detector.samples[9] == 14);
+    //REQUIRE(detector.samples.reserve() == 10);
+    //for(int i = 0; i < 15; i++){
+    //    detector.add_sample(i);
+    //}
+    REQUIRE(detector.samples.size() == 0);
+    //REQUIRE(detector.samples.reserve() == 0);
+    //REQUIRE(detector.samples[0] == 5);
+    //REQUIRE(detector.samples[1] == 6);
+    //REQUIRE(detector.samples[2] == 7);
+    //REQUIRE(detector.samples[3] == 8);
+    //REQUIRE(detector.samples[4] == 9);
+    //REQUIRE(detector.samples[5] == 10);
+    //REQUIRE(detector.samples[6] == 11);
+    //REQUIRE(detector.samples[7] == 12);
+    //REQUIRE(detector.samples[8] == 13);
+    //REQUIRE(detector.samples[9] == 14);
 }
 
 TEST_CASE("HPS_Frequency_Detector::detect"){
@@ -65,7 +66,7 @@ TEST_CASE("HPS_Frequency_Detector::detect"){
 
     // Verify Size is 0 and buffer has 256 available
     REQUIRE(detector.samples.size() == 0);
-    REQUIRE(detector.samples.reserve() == 256);
+    //REQUIRE(detector.samples.reserve() == 256);
 
     for(int i = 0; i < 256; i++){
         detector.add_sample(bagpipe3300[i]);
@@ -77,7 +78,7 @@ TEST_CASE("HPS_Frequency_Detector::detect"){
 
     // Verify Buffer is full
     REQUIRE(detector.samples.size() == 256);
-    REQUIRE(detector.samples.reserve() == 0);
+    //REQUIRE(detector.samples.reserve() == 0);
 
     // This sample set should give frequency 455.5
     //REQUIRE(detector.detect() == 455.5);
@@ -90,7 +91,44 @@ TEST_CASE("HPS_Frequency_Detector::hanning"){
     }
 
     detector.hanning();
-    REQUIRE(detector.samples.reserve() == 0);
+    //REQUIRE(detector.samples.reserve() == 0);
     REQUIRE(detector.samples[0] == 0);
-    REQUIRE(detector.samples[256] == 0);
+    REQUIRE(detector.samples[255] == 0);
+}
+
+TEST_CASE("HPS_Frequency_Detector::RealFFT"){
+    std::vector<double> data = {323, 493, 466, 586, 360, 509, 403, 396, 654, 249, 600, 508, 410, 452, 412, 427, 305, 552, 612, 36,
+                                424, 751, 230, 415, 483, 523, 342, 728, 336, 370, 539, 480, 588, 310, 456, 420, 366, 640, 192, 604,
+                                589, 395, 405, 483, 361, 395, 553, 571, 29,  439, 726, 217, 411, 465, 555, 383, 747, 301, 404, 575, 
+                                477, 587, 302, 428, 430, 330, 644, 189, 596, 634, 375, 394, 547, 313, 444, 544, 540, 52,  440, 679, 
+                                214, 414, 470, 594, 379, 754, 314, 420, 605, 455, 556, 325, 411, 426, 305, 631, 202, 589, 648, 354, 
+                                385, 588, 295, 454, 545, 520, 86,  435, 659, 218, 421, 495, 626, 349, 752, 335, 430, 620, 433, 527, 
+                                363, 411, 420, 306, 610, 213, 599, 650, 348, 374, 618, 300, 448, 556, 509, 102, 430, 663, 221, 426, 
+                                511, 639, 312, 747, 344, 431, 623, 409, 504, 380, 410, 414, 313, 609, 226, 599, 636, 358, 369, 600,
+                                333, 438, 555, 510, 95,  426, 702, 204, 423, 524, 630, 295, 742, 351, 426, 617, 413, 499, 389, 410, 
+                                417, 325, 617, 234, 599, 638, 361, 378, 591, 344, 436, 565, 525, 79,  430, 719, 196, 423, 525, 615, 
+                                294, 735, 350, 419, 618, 413, 505, 383, 410, 418, 337, 622, 241, 577, 640, 367, 374, 583, 339, 427, 
+                                570, 520, 67,  428, 726, 188, 422, 525, 591, 301, 732, 345, 412, 615, 427, 510, 376, 416, 420, 349, 
+                                617, 242, 555, 652, 373, 379, 574, 336, 424, 580, 522, 63,  427, 733, 200, 421};
+    // Create HPS_Frequency_Detector with window size 256, 3 products
+    HPS_Frequency_Detector detector(256, 3);
+
+    // Verify Size is 0 and buffer has 256 available
+    REQUIRE(detector.samples.size() == 0);
+    //REQUIRE(detector.samples.reserve() == 256);
+
+    for(int i = 0; i < 256; i++){
+        detector.add_sample(data[i]);
+        // Verify frequency detected as 0 if buffer isn't full
+        if(i == 254){
+            REQUIRE(detector.detect() == 0);
+        }
+    }
+
+    auto ret_val = detector.detect();
+    //for(int i = 0; i < detector.samples.size(); i++){
+    //    std::cout << detector.samples[i] << std::endl;//", ";
+    //    //if(i % 10 == 0){ std::cout << std::endl; }
+    //}
+    std::cout << ret_val << std::endl;
 }
